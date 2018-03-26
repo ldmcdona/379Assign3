7d211eb71b8f7335b5be937ba354b8828965131f
ldmcdona <ldmcdona@ualberta.ca>
2018-03-26 14:53:34 -0600

    clears and decrements

---
+++ hash4.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  int hash;
+  char* a;
+  char* d;
+  char* e;
+  char* f;
+  int x = 0;
+  int y = 0;
+
+  a = "X";
+  d = "P";
+  e = " ";
+  f = "C";
+
+  printf("Enter hash size: ");
+  scanf("%d", &hash);
+  if(hash < 1){
+    printf("Invalid hash size\n");
+    return 0;
+  }
+  printf("You entered %d\n", hash);
+
+  char b[20];
+  int c[hash];
+
+  for(int i=0; i<20; i++){
+    b[i] = '!';
+  }
+  for(int i=0; i<hash; i++){
+    c[i] = 0;
+  }
+    
+  printf("Enter X to end the program.\n");
+  printf("Enter P to print the hash table\n");
+  printf("Enter C to clear the hash table\n");
+  printf("Begin your string with * to decrement\n");
+  printf("Enter a string to hash: ");
+
+  while(scanf("%s", b) != EOF){
+    if(strcmp(a, b) == 0){
+      break;
+    }else if(strcmp(d, b) == 0){
+      for(int i=0; i<hash; i++){
+        printf("hash[%d] has been hit %d times\n", i, c[i]);
+      }
+    }else if(strcmp(f, b) == 0){
+      for(int i=0; i<hash; i++){
+	c[i] = 0;
+	printf("Hash table cleared.\n");
+      }
+    }else{
+      if(b[0] == '*'){
+	for(int j=1; j<20; j++){
+	  if(b[j] != '!'){
+	    x += (int)b[j];
+	  }
+	}
+	y = x % hash;
+	c[y]--;
+	printf("You entered: '%s', which hashes to %d\n", b, y);
+    }else{
+	for(int j=0; j<20; j++){
+	  if(b[j] != '!'){
+	    x += (int)b[j];
+	  }
+	}
+	y = x % hash;
+	c[y]++;
+	printf("You entered: '%s', which hashes to %d\n", b, y);
+      }
+      for(int j=0; j<20; j++){
+	b[j] = '!';
+      }
+    }
+    x = 0;
+    printf("Enter a string to hash: ");
+  }
+  
+  return 0;
+}
