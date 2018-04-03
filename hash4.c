1070b329329339cb40f70f20f14906073b7a2c9a
Craig Lindsay <Craig Lindsay>
2018-04-03 13:31:26 -0600

    hahahahahah

--- hash4.c
+++ hash4.c
@@ -2,14 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-  int hash;
+int hash;
+int* c;
+
+void clearTheTable() {
+  for(int i=0; i<hash; i++){
+    c[i] = 0;
+  }
+}
+
+void initHashTable(int hashSize) {
+  hash = hashSize;
+  c = (int*)malloc(sizeof(int)*hash);
+  clearTheTable();
+}
+
+void printTheTable() {
+  for(int i=0; i<hash; i++){
+    printf("hash[%d] has been hit %d times\n", i, c[i]);
+  }
+}
+
+int hashf(char* toHash) {
+  int x = 0;
+  for(int j=0; j<strlen(toHash); j++){
+    x += (int)toHash[j];
+  }
+  return x % hash;
+}
+
+void decrement(int boxNo) {
+  c[boxNo]--;
+}
+
+void increment(int boxNo) {
+  c[boxNo]++;
+}
+
+void end() {
+  free(c);
+}
+
+int main() {
+  int hashBuff;
   char* a;
   char* d;
   char* e;
   char* f;
-  int x = 0;
-  int y = 0;
 
   a = "X";
   d = "P";
@@ -17,23 +56,22 @@ int main(){
   f = "C";
 
   printf("Enter hash size: ");
-  scanf("%d", &hash);
-  if(hash < 1){
+  scanf("%d", &hashBuff);
+  if(hashBuff < 1){
     printf("Invalid hash size\n");
     return 0;
   }
+  initHashTable(hashBuff);
   printf("You entered %d\n", hash);
 
+  //Specialmode
   char b[20];
-  int c[hash];
 
   for(int i=0; i<20; i++){
     b[i] = '!';
   }
-  for(int i=0; i<hash; i++){
-    c[i] = 0;
-  }
-    
+  //######
+
   printf("Enter X to end the program.\n");
   printf("Enter P to print the hash table\n");
   printf("Enter C to clear the hash table\n");
@@ -42,41 +80,22 @@ int main(){
 
   while(scanf("%s", b) != EOF){
     if(strcmp(a, b) == 0){
+      end();
       break;
     }else if(strcmp(d, b) == 0){
-      for(int i=0; i<hash; i++){
-        printf("hash[%d] has been hit %d times\n", i, c[i]);
-      }
+      printTheTable();
     }else if(strcmp(f, b) == 0){
-      for(int i=0; i<hash; i++){
-	c[i] = 0;
-	printf("Hash table cleared.\n");
-      }
+      clearTheTable();
     }else{
       if(b[0] == '*'){
-	for(int j=1; j<20; j++){
-	  if(b[j] != '!'){
-	    x += (int)b[j];
-	  }
-	}
-	y = x % hash;
-	c[y]--;
-	printf("You entered: '%s', which hashes to %d\n", b, y);
-    }else{
-	for(int j=0; j<20; j++){
-	  if(b[j] != '!'){
-	    x += (int)b[j];
-	  }
-	}
-	y = x % hash;
-	c[y]++;
-	printf("You entered: '%s', which hashes to %d\n", b, y);
+	decrement(hashf(memmove(b, b+1, 20)));
+      }else{
+	increment(hashf(b));
       }
       for(int j=0; j<20; j++){
 	b[j] = '!';
       }
     }
-    x = 0;
     printf("Enter a string to hash: ");
   }
   
