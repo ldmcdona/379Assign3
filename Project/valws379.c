8301945928867a6523df2242a4b97d19fa841c62
Craig Lindsay <Craig Lindsay>
2018-04-05 22:36:17 -0600

    made the plot wider

--- Project/valws379.c
+++ Project/valws379.c
@@ -93,7 +93,7 @@ int main(int argc, char* argv[])
   int DTI = digitsToInclude(pgSize);
 
   gnuplotPipe = popen ("gnuplot", "w");
-  fprintf(gnuplotPipe, "set terminal pngcairo  transparent enhanced font 'arial,10' fontscale 1.0 size 600, 400\n");
+  fprintf(gnuplotPipe, "set terminal pngcairo  transparent enhanced font 'arial,10' fontscale 1.0 size 2000, 400\n");
   fprintf(gnuplotPipe, "set output 'test.png'\n");
   fprintf(gnuplotPipe, "plot '-' \n");
 
